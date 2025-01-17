90c73e7f021190153a2fbd29cacb0d98748bbb0c
FredyAlexHoyosAriza <donotgobehind@gmail.com>
2025-01-17 18:59:25 -0500

    Se deshacen cambios de configuración adicionales; metricas, etc, en server en route.ts

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -2,9 +2,18 @@ import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { schema } from '@/api/server/schema';
 import { NextRequest } from 'next/server';
+import { ApolloServerPluginUsageReporting } from '@apollo/server/plugin/usageReporting';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 // En la arquitectura MVC este archivo representaría una VISTA de un solo endpoint
 // Inicialización del servidor Apollo
+// const server = new ApolloServer({ schema,
+//   introspection: true,
+//   plugins: [
+//     ApolloServerPluginUsageReporting(),
+//     ApolloServerPluginLandingPageLocalDefault({ embed: true }), // Activa el sandbox
+//   ],
+// });
 const server = new ApolloServer({ schema });
 const handler = startServerAndCreateNextHandler(server);
 
