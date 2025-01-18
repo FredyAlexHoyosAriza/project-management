067a39cfb43f232d44f38058ecd3100c569c7065
FredyAlexHoyosAriza <donotgobehind@gmail.com>
2025-01-17 22:07:59 -0500

    Se agrega middleware para manejo de cors en graphql/route.ts

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -8,13 +8,6 @@ import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin
 
 // En la arquitectura MVC este archivo representaría una VISTA de un solo endpoint
 // Inicialización del servidor Apollo
-// const server = new ApolloServer({ schema,
-//   introspection: true,//permite uso apollo sandbox
-//   plugins: [
-//     ApolloServerPluginUsageReporting(),//permite registro de métricas; estadística
-//     ApolloServerPluginLandingPageLocalDefault({ embed: true }), // Activa el sandbox incluso en producción; vercel despliegue
-//   ],
-// });
 const server = new ApolloServer({ schema,
   introspection: true,//permite uso apollo sandbox
     plugins: [
@@ -24,18 +17,37 @@ const server = new ApolloServer({ schema,
  });
 // const handler = startServerAndCreateNextHandler(server);
 const allowedOrigins = ['https://studio.apollographql.com']; // Dominios permitidos
+const corsOptions = {
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+// Inicializa CORS correctamente
+const corsMiddleware = cors(corsOptions);
 
 const handler = startServerAndCreateNextHandler(server, {
   context: async (req, res) => {
-    // Configurar CORS
-    cors({
-      origin: allowedOrigins, // Solo Apollo Studio
-      methods: ['GET', 'POST', 'OPTIONS'], // Métodos permitidos
-      allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
-    })(req, res, () => {});
-    return { req, res };
+    return new Promise((resolve, reject) => {
+      corsMiddleware(req, res, (err) => {
+        if (err) return reject(err);
+        resolve({ req, res });
+      });
+    });
   },
 });
+
+// const handler = startServerAndCreateNextHandler(server, {
+//   context: async (req, res) => {
+//     // Configurar CORS
+//     cors({
+//       origin: allowedOrigins, // Solo Apollo Studio
+//       methods: ['GET', 'POST', 'OPTIONS'], // Métodos permitidos
+//       allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+//     })(req, res, () => {});
+//     return { req, res };
+//   },
+// });
 // Rutas GET y POST para el endpoint GraphQL
 // Ir a la ruta http://localhost:3000/api/graphql implica hacer un GET al endpoint:
 // El metodo GET se requiere para abrir el Apollo SANDBOX
