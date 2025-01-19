d79860df9811af9f11c3bb8b5bc95f4f7a9e3b61
FredyAlexHoyosAriza <donotgobehind@gmail.com>
2025-01-18 21:51:33 -0500

    Pruebo una configuración para plugin que incluyen el apollo_graph_ref

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -3,25 +3,26 @@ import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { schema } from '@/api/graphql';
 import { NextRequest } from 'next/server';
 import { ApolloServerPluginUsageReporting } from '@apollo/server/plugin/usageReporting';
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ApolloServerPluginLandingPageLocalDefault, ApolloServerPluginLandingPageProductionDefault } from '@apollo/server/plugin/landingPage/default';
 
 // En la arquitectura MVC este archivo representaría una VISTA de un solo endpoint
 // Inicialización del servidor Apollo
 const server = new ApolloServer({ schema
   ,introspection: true,//permite uso apollo sandbox
-    plugins: [
-      ApolloServerPluginUsageReporting(),//permite registro de métricas; estadística
-      ApolloServerPluginLandingPageLocalDefault({ embed: true }), // Activa el sandbox incluso en producción; vercel despliegue
-    ],
     // plugins: [
-    //   // Install a landing page plugin based on NODE_ENV
-    //   process.env.NODE_ENV === 'production'
-    //     ? ApolloServerPluginLandingPageProductionDefault({
-    //         graphRef: 'my-graph-id@my-graph-variant',
-    //         footer: false,
-    //       })
-    //     : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+    //   ApolloServerPluginUsageReporting(),//permite registro de métricas; estadística
+    //   ApolloServerPluginLandingPageLocalDefault({ embed: true }), // Activa el sandbox incluso en producción; vercel despliegue
     // ],
+    plugins: [
+      ApolloServerPluginUsageReporting(),
+      // Install a landing page plugin based on NODE_ENV
+      process.env.NODE_ENV === 'production'
+        ? ApolloServerPluginLandingPageProductionDefault({
+            graphRef: 'PMG-7o2qaf@current',
+            footer: false,
+          })
+        : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
+    ],
  });
 const handler = startServerAndCreateNextHandler(server);
 
