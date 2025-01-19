45a80250557bda614f15d7e40ce1614a7a31d8a9
FredyAlexHoyosAriza <donotgobehind@gmail.com>
2025-01-18 21:18:29 -0500

    Realizo un cambio en los plugins del servidor para probar si se afecta la conección con apollo studio

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -3,16 +3,25 @@ import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { schema } from '@/api/graphql';
 import { NextRequest } from 'next/server';
 // import cors from "cors";
-import { ApolloServerPluginUsageReporting } from '@apollo/server/plugin/usageReporting';
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+// import { ApolloServerPluginUsageReporting } from '@apollo/server/plugin/usageReporting';
+import { ApolloServerPluginLandingPageLocalDefault, ApolloServerPluginLandingPageProductionDefault } from '@apollo/server/plugin/landingPage/default';
 
 // En la arquitectura MVC este archivo representaría una VISTA de un solo endpoint
 // Inicialización del servidor Apollo
 const server = new ApolloServer({ schema
   ,introspection: true,//permite uso apollo sandbox
+    // plugins: [
+    //   ApolloServerPluginUsageReporting(),//permite registro de métricas; estadística
+    //   ApolloServerPluginLandingPageLocalDefault({ embed: true }), // Activa el sandbox incluso en producción; vercel despliegue
+    // ],
     plugins: [
-      ApolloServerPluginUsageReporting(),//permite registro de métricas; estadística
-      ApolloServerPluginLandingPageLocalDefault({ embed: true }), // Activa el sandbox incluso en producción; vercel despliegue
+      // Install a landing page plugin based on NODE_ENV
+      process.env.NODE_ENV === 'production'
+        ? ApolloServerPluginLandingPageProductionDefault({
+            graphRef: 'my-graph-id@my-graph-variant',
+            footer: false,
+          })
+        : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
     ],
  });
 const handler = startServerAndCreateNextHandler(server);
