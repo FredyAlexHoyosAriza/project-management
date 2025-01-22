fc1cf066fdb537286e42897dc493a0834d1ee8a3
FredyAlexHoyosAriza <donotgobehind@gmail.com>
2025-01-22 02:47:31 -0500

    Se quitan cambios que incluian cors

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,7 +1,7 @@
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { schema } from '@/api/graphql';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 import { ApolloServerPluginUsageReporting } from '@apollo/server/plugin/usageReporting';
 import { ApolloServerPluginLandingPageLocalDefault, ApolloServerPluginLandingPageProductionDefault } from '@apollo/server/plugin/landingPage/default';
 
@@ -29,40 +29,9 @@ const handler = startServerAndCreateNextHandler(server);
 // Rutas GET y POST para el endpoint GraphQL
 // Ir a la ruta http://localhost:3000/api/graphql implica hacer un GET al endpoint:
 // El metodo GET se requiere para abrir el Apollo SANDBOX
-// export async function GET(req: NextRequest): Promise<Response> {
-//   return handler(req);
-// }
-// export async function POST(req: NextRequest): Promise<Response> {
-//   return handler(req);
-// }
-
-// Configuración CORS
-const ALLOWED_ORIGIN = 'https://studio.apollographql.com';
-
-function setCORSHeaders(response: Response) {
-  response.headers.set('Access-Control-Allow-Origin', ALLOWED_ORIGIN);
-  response.headers.set('Access-Control-Allow-Credentials', 'true');
-  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-}
-
-// Manejo de preflight (OPTIONS)
-export async function OPTIONS(): Promise<Response> {
-  const response = NextResponse.json(null, { status: 204 });
-  setCORSHeaders(response);
-  return response;
-}
-
-// Manejo de solicitudes GET
 export async function GET(req: NextRequest): Promise<Response> {
-  const response = await handler(req);
-  setCORSHeaders(response);
-  return response;
+  return handler(req);
 }
-
-// Manejo de solicitudes POST
 export async function POST(req: NextRequest): Promise<Response> {
-  const response = await handler(req);
-  setCORSHeaders(response);
-  return response;
+  return handler(req);
 }
