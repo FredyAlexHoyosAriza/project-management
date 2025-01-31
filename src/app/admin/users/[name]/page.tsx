261702477d4c2d6f711679e7ca386d9935698ae8
FredyAlexHoyosAriza <donotgobehind@gmail.com>
2025-01-31 17:42:57 -0500

    Se crea ruta de para edición de usuario admin/users/edit:<nombre-usuario> o admin/users/<nombre-usuario>

--- src/app/admin/users/[name]/page.tsx
+++ src/app/admin/users/[name]/page.tsx
@@ -1,5 +1,8 @@
 export default async function EditUserPage({params}: {params: Promise<{ name: string }>}) {
-  const { name } = await params;
+  let { name } = await params;
+  if (name.includes('%3A')) {//edit%3A
+    name = name.split('%3A')[1];
+  }
     return (
       <div>
         <h1>Editando usuario con nombre: {name}</h1>
